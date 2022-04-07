576300258900ebcb2c892d1cf90f04409019a519
umutaltinsoy <umutcaltinsoy@gmail.com>
2022-04-07 05:54:32 +0300

    dto converters

---
+++ src/main/java/com/altinsoy/account/dto/CustomerAccountDtoConverter.java
@@ -0,0 +1,28 @@
+package com.altinsoy.account.dto;
+
+import com.altinsoy.account.model.Account;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+@Component
+public class CustomerAccountDtoConverter {
+
+    private final TransactionDtoConverter transactionDtoConverter;
+
+    public CustomerAccountDtoConverter(TransactionDtoConverter converter) {
+        this.transactionDtoConverter = converter;
+    }
+
+    public CustomerAccountDto convert(Account from) {
+        return new CustomerAccountDto(
+                Objects.requireNonNull(from.getId()),
+                from.getBalance(),
+                from.getTransaction()
+                        .stream()
+                        .map(transactionDtoConverter::convert)
+                        .collect(Collectors.toSet()),
+                from.getCreationDate());
+    }
+}
